c3c8136c874e7307f398c85f5c99a4ee12f2a9e1
AI-UNS <64642455+AI-UNS@users.noreply.github.com>
2020-08-19 14:54:50 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,37 @@
+$(document).ready(function () {
+    
+    
+
+    // $('.play-button').click(function () { 
+        
+
+    //     $('.play-button').remove();
+    //     $('.process__video').attr('autoplay' , 'true');
+     
+            
+    
+    // });
+
+
+ 
+
+    // $('.rotate-effect').click(function () { 
+    //     // $('.rotate-effect').toggle();
+    //     // $('.whole__square').css("transform","rotate(-90deg)" );
+    //     // $('.whole__square').css("transform","rotate(-90deg)" );
+    //     $('.whole__square').addClass('rotator')
+        
+    //     // $(selector).addClass(className);
+        
+    // });
+    
+    
+    // $('.rotate-effect-rever').click(function () { 
+    //     $('.whole__square').css("transform","rotate(90deg)" );
+       
+        
+    // });
+
+    
+
+});
